680672641f27beba84ab69212392a90b476dc9a0
OlegBabitski <78315310+OlegBabitski@users.noreply.github.com>
2021-03-18 15:57:22 +0300

    Update Discharges.cpp

--- Discharges.cpp
+++ Discharges.cpp
@@ -1,13 +1,15 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
-void flipNumber(int num);
+int numberOfCharacters (int number);
+void flipNumber (int num);
 
 int main()
 {
 	int number;
 
-	cout << "Let's flip your number!!!" << endl;
+	cout << "Let's flip your number!!!" << endl << endl;
 	cout << "Enter the number: ";
 	cin >> number;
 
@@ -17,12 +19,35 @@ int main()
 	return 0;
 }
 
-void flipNumber(int num) {
-	int a;
-
-	while (num > 0) {
-		a = num % 10;
-		cout << a;
-		num /= 10;
+int numberOfCharacters (int number){
+	int counter = 0, discharge = 10;
+	
+	while (number > 0) {
+		number /= 10;
+		counter++;
 	}
+	
+	counter -= 1; 
+	
+	for (int i = 1; i < counter; i++){
+		discharge *= 10;
+	}
+	
+	return discharge;
 }
+
+void flipNumber(int num){
+	int a, discharge = numberOfCharacters (num), reverse = 0;
+
+	while (discharge > 0) {
+		a = num % 10;
+		a = a * discharge;
+		discharge /= 10;
+		num /= 10;
+		reverse += a;
+	}
+	
+	cout << reverse << endl;
+}
+
+
